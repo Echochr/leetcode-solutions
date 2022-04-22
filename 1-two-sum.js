bd711d73998752b90159c890d8d839dea055213a
Chris Ho <chris.ho@technet-asia.com>
2022-04-22 17:35:06 +0800

    add comments for revision

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -1,10 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 const twoSum = (nums, target) => {
+  // declare a new Map
   const hash = new Map();
+  // iterate through the array once
   for (let i = 0; i < nums.length; i++) {
+    // check if the Map has the diff key-value pair that could add up to the target
     const diff = target - nums[i];
     if (hash.has(diff)) {
+      // return the two indices of the values that add up to the target
       return [hash.get(diff), i];
     }
+    // otherwise add a new key-value pair to the map { value : index }
     hash.set(nums[i], i);
   }
 };
--- 217-contains-duplicate.js
+++ 217-contains-duplicate.js
@@ -3,10 +3,14 @@
  * @return {boolean}
  */
  const containsDuplicate = (nums) => {
+    // declare a new Set
     const set = new Set();
     for (const num of nums) {
+        // check if value already exists in the set
         if (set.has(num)) return true;
+        // if not, add it to the set
         set.add(num);
     }
+    // finally return false if all values are unique
     return false;
 }
