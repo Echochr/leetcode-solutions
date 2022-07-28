f775b44717ea5f3fb05d98d7fa713c608908b891
Chris Ho <chris.ho@technet-asia.com>
2022-07-28 17:59:31 +0800

    217-contains-duplicate.ts

---
+++ solutions/217-contains-duplicate.ts
@@ -0,0 +1,9 @@
+const containsDuplicate = (nums: number[]): boolean => {
+  const set = new Set();
+  for (const num of nums) {
+    if (set.has(num))
+      return true;
+    set.add(num);
+  }
+  return false;
+};
